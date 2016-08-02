504f071df276da2dc23a16cc31ceb169e0da5b4c
algosolv <vkumar.vinay@ymail.com>
2016-08-02 23:53:45 +0530

    Add files via upload

--- app/src/main/java/com/algosolv/parkingsolv/MainActivity.java
+++ app/src/main/java/com/algosolv/parkingsolv/MainActivity.java
@@ -4,16 +4,10 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.net.Uri;
-import android.os.Bundle;
-import android.provider.Settings;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,68 +18,59 @@ import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-
     Button login;
     TextView signup;
     EditText mobileno;
     String mobilenumber;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         login = (Button) findViewById(R.id.loginbutton);
         signup = (TextView) findViewById(R.id.signuptextview);
         mobileno = (EditText) findViewById(R.id.mobileedittext);
-        mobilenumber = mobileno.getText().toString();
-        login.setOnClickListener(this);
-        signup.setOnClickListener(this);
 
+        login.setOnClickListener(this);
+
+        signup.setOnClickListener(this);
         // GPS enable settings
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+        {
             Toast.makeText(getApplicationContext(), "GPS Enabled", Toast.LENGTH_SHORT).show();
-        } else {
+        }
+        else
+        {
             showGPSDisabledAlertToUser();
         }
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-
+    @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.loginbutton) {
-            /*String pattern = "[0-9]+";
 
-            // Create a Pattern object
-            Pattern r = Pattern.compile(pattern);
-
-            // Now create matcher object.
-            Matcher m = r.matcher(mobilenumber);*/
-            if (this.mobilenumber.length() < 9 || this.mobilenumber == "") {
-                Toast.makeText(getApplicationContext(), "Enter a valid Phone Number:"+this.mobilenumber+"?", Toast.LENGTH_LONG).show();
-            } else {
-                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+        if(v.getId()==R.id.loginbutton)
+        {
+            mobilenumber = mobileno.getText().toString();
+            if(mobilenumber.length()<9 || mobilenumber=="")
+            {
+                Toast.makeText(getApplicationContext(),"Enter valid Phone Number",Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
                 startActivity(intent);
                 finish();
             }
         }
-        if (v.getId() == R.id.signuptextview) {
-            Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+        if(v.getId()==R.id.signuptextview)
+        {
+            Intent intent = new Intent(MainActivity.this,MapsActivity.class);
             startActivity(intent);
             finish();
         }
     }
-
     private void showGPSDisabledAlertToUser() {
         /*
         Intent intent=new Intent("android.location.GPS_ENABLED_CHANGE");
@@ -111,7 +96,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 Intent callGPSSettingIntent = new Intent(
-                                        Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                                 startActivity(callGPSSettingIntent);
                             }
                         });
@@ -124,44 +109,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         AlertDialog alert = alertDialogBuilder.create();
         alert.show();
     }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.algosolv.parkingsolv/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
     }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.algosolv.parkingsolv/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
-}
